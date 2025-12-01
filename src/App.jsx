c1bd3f0c601de18ebfcad083397011e2a699238d
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-12-01 12:58:57 +0100

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,49 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const [addedProducts, setAddeddProducts] = useState([])
+
+  const addToCart = product => {
+    const isProductAdded = addedProducts.some(p => p.name === product.name);
+    if (isProductAdded) {
+      return
+    }
+
+    setAddeddProducts(curr => [...curr, {
+      ...product,
+      quantity: 1
+    }])
+  }
+
   return (
     <>
       <h1>La mia lista</h1>
       <ul>
-        {products.map((product) => {
+        {products.map((product, i) => {
           return (
-            <li><span>{product.name}</span>
+            <li key={i}>
+              <span>{product.name}</span>
               <p>{product.price.toFixed(2)}€</p>
+              <button onClick={() => { addToCart(product) }}>Aggiungi al carrello</button>
             </li>
+
           )
         })}
-
       </ul>
+      {addedProducts.length > 0 && (
+        <>
+          <h2>Il mio carrello</h2>
+          <ul>
+            {addedProducts.map((product, i) => {
+              return (
+                <li key={i}>
+                  <p>{product.name + " " + (product.price.toFixed(2)) + "€ x" + product.quantity}</p>
+                </li>
+              )
+            })}
+          </ul>
+        </>
+      )}
     </>
   )
 }
